b8d50c0e90819a04ffe5d99c3e995a782b17b7e8
sangjun121 <129354526+sangjun121@users.noreply.github.com>
2024-07-07 21:52:56 +0900

    [Silver IV] Title: 베스트셀러, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        String[] sales = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            sales[i] = br.readLine();
+        }
+
+        //1. 구매도서 정렬
+        Arrays.sort(sales);
+
+        //2. 최대 구매 수량 저장
+        int count = 1;
+        int maxCount = 1;
+        String maxBook = sales[0];
+
+        for(int i = 1; i < N; i++) {
+            if(sales[i].equals(sales[i-1])){
+                count++;
+            }
+            else{
+                if(maxCount == count){
+                    if(maxBook.compareTo(sales[i-1]) > 0)
+                        maxBook = sales[i-1];
+                }
+                else if(maxCount < count){
+                    maxCount = count;
+                    maxBook = sales[i-1];
+                }
+                count = 1;
+            }
+        }
+
+        //마지막 원소처리
+        if(maxCount == count){
+            if(maxBook.compareTo(sales[N-1]) > 0)
+                maxBook = sales[N-1];
+        }
+        else if(maxCount < count){
+            maxCount = count;
+            maxBook = sales[N-1];
+        }
+
+        bw.write(maxBook);
+        bw.close();
+    }
+}
